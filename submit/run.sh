0aa91e8abdf88943ba32bf8848bab895869cbbc6
geekyouth <forsupergeeker@gmail.com>
2022-12-26 15:14:53 +0800

    补充一个完整的单元测试

--- submit/run.sh
+++ submit/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-$HADOOP_HOME/bin/hadoop jar $JAR_FILEPATH $CLASS_TO_RUN $PARAMS
+# 等待 hdfs 自动退出安全模式，防止提交报错
+hdfs dfsadmin -safemode wait
+# 生成临时目录，防止已存在的目录导致任务失败
+TMP=$(date +%s); echo TMP=$TMP
+hdfs dfs -mkdir -p /input
+hdfs dfs -put $HADOOP_CONF_DIR/* /input
+# input/* 包含所有子目录下的文件，防止报错
+hadoop jar $JAR_FILEPATH $CLASS_TO_RUN /input/* /output/$TMP
+hdfs dfs -cat /output/$TMP/*
