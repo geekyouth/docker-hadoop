10a80ebaf864cc493ee1a3af8e7615b8c00bc932
geekyouth <forsupergeeker@gmail.com>
2022-12-26 15:55:22 +0800

    生成临时目录（"yyyymmdd-hhmmss.SSS"），防止已存在的目录导致任务失败

--- submit/run.sh
+++ submit/run.sh
@@ -2,10 +2,10 @@
 
 # 等待 hdfs 自动退出安全模式，防止提交报错
 hdfs dfsadmin -safemode wait
-# 生成临时目录，防止已存在的目录导致任务失败
-TMP=$(date +%s); echo TMP=$TMP
-hdfs dfs -mkdir -p /input
-hdfs dfs -put $HADOOP_CONF_DIR/* /input
-# input/* 包含所有子目录下的文件，防止报错
-hadoop jar $JAR_FILEPATH $CLASS_TO_RUN /input/* /output/$TMP
+# 生成临时目录（"yyyymmdd-hhmmss.SSS"），防止已存在的目录导致任务失败
+TMP=$(date +%Y%m%d-%H%M%S.%3N); echo TMP=$TMP
+hdfs dfs -mkdir -p /input/$TMP
+hdfs dfs -put $HADOOP_CONF_DIR/* /input/$TMP
+# input/$TMP/* 包含所有子目录下的文件，防止报错
+hadoop jar $JAR_FILEPATH $CLASS_TO_RUN /input/$TMP/* /output/$TMP
 hdfs dfs -cat /output/$TMP/*
