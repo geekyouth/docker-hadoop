8367bed020b413326c685dc2115fd6f4935ad015
geekyouth <forsupergeeker@gmail.com>
2022-12-26 15:08:54 +0800

    historyserver 升级为 timelineserver

--- historyserver/run.sh
+++ historyserver/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR historyserver
+# > yarn historyserver
+#DEPRECATED: Use of this command to start the timeline server is deprecated.
+#Instead use the timelineserver command for it.
+yarn --config $HADOOP_CONF_DIR timelineserver
