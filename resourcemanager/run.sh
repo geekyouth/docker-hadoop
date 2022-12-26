13801c4c47da12a4632a8d51136b79609dce20d5
geekyouth <forsupergeeker@gmail.com>
2022-12-26 15:11:33 +0800

    等待 hdfs 自动退出安全模式，防止 resourcemanager 报错

--- resourcemanager/run.sh
+++ resourcemanager/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
+# 等待 hdfs 自动退出安全模式，防止 resourcemanager 报错
+hdfs dfsadmin -safemode wait
+yarn --config $HADOOP_CONF_DIR resourcemanager
